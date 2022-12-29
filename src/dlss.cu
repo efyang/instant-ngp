7ddd0429d0e13e050e4ea04a27a13596093de371
Edward Yang <edward.yang6771@gmail.com>
2022-12-29 02:14:04 -0600

    Working texture rendering

--- src/dlss.cu
+++ src/dlss.cu
@@ -521,7 +521,7 @@ public:
 
 		image_info.tiling = VK_IMAGE_TILING_OPTIMAL;
 		image_info.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-		image_info.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
+		image_info.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT;
 		image_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 		image_info.samples = VK_SAMPLE_COUNT_1_BIT;
 		image_info.flags = 0;
